35d9110569cd4bb77b6dc3809fcec1e93ab3b56d
Huyền Trang <98087513+PhamVuHuyenTrang@users.noreply.github.com>
2023-01-25 11:17:30 +0700

    Update Semaphore-based barrier.py

--- Semaphore-based barrier.py
+++ Semaphore-based barrier.py
@@ -1,7 +1,7 @@
 from threading import * 
 import time
-from random import randint
-
+from random import randint, shuffle
+n = int(input("Enter the number of threads: "))
 counter = 0
 arrival = Semaphore(1)
 departure = Semaphore(0)
@@ -12,7 +12,7 @@ def barrier(thread_name):
     print(thread_name + " has reached the barrier.")
     arrival.acquire()
     counter += 1
-    if counter < 6:
+    if counter < n:
         arrival.release()
     else:
         departure.release()
@@ -28,17 +28,13 @@ def barrier(thread_name):
         print("All threads have been unlocked")
         
 # creating multiple thread   
-thread_1 = Thread(target = barrier , args = ('Thread 1',))  
-thread_2 = Thread(target = barrier, args = ('Thread 2',))  
-thread_3 = Thread(target = barrier , args = ('Thread 3',))  
-thread_4 = Thread(target = barrier , args = ('Thread 4',))  
-thread_5 = Thread(target = barrier , args = ('Thread 5',))  
-thread_6 = Thread(target = barrier , args = ('Thread 6',))  
-  
-# calling the threads   
-thread_1.start()  
-thread_2.start()  
-thread_3.start()  
-thread_4.start()  
-thread_5.start()  
-thread_6.start()  
+threads = []
+for i in range(n):
+    thread_name = "Thread " + str(i+1)
+    thread = Thread(target= barrier, args = (thread_name,))
+    threads.append(thread)
+    
+shuffle(threads)
+
+for thread in threads:
+    thread.start()
