b63de3c012df8c90069266d94704af7164bb8816
Vu Hoang <61404697+lggvu@users.noreply.github.com>
2023-02-15 23:22:42 +0700

    gui

---
+++ run_algorithm.py
@@ -0,0 +1,14 @@
+def run_test_and_set():
+    '''
+    run the algorithm, returns a dataframe with the format similar to `test-and-set.csv` file
+    '''
+    return "test and set was called."
+
+def run_semaphore():
+    return "semaphore was called."
+
+def run_tree_based():
+    return "tree-based algorithm was called."
+
+def run_centralized():
+    return "centralized algorithm was called."
